5f44769afa0e4aebf79b4d4a6e6058fc856e6c15
Takuya Abe <takuya.abe@linecorp.com>
2022-03-23 16:53:16 +0900

    style: Apply lint and prettier

--- 01_money/src/Sample.ts
+++ 01_money/src/Sample.ts
@@ -1,8 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
 export class Sample {
   amount: number = 0;
+
   constructor(amount: number) {
     this.amount = amount;
-    const dollar = 0;
-    const mesage = "double quote?";
   }
 }
