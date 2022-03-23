74ce63ec9dd8f969334efd515cf108bff72aac1c
Takuya Abe <takuya.abe@linecorp.com>
2022-03-23 16:27:23 +0900

    chore: Install pretteir

--- 01_money/src/Sample.ts
+++ 01_money/src/Sample.ts
@@ -1,7 +1,8 @@
 export class Sample {
-    amount: number = 0;
-    constructor(amount: number) {
-        this.amount = amount;
-        const dollar = 0;
-    }
+  amount: number = 0;
+  constructor(amount: number) {
+    this.amount = amount;
+    const dollar = 0;
+    const mesage = "double quote?";
+  }
 }
