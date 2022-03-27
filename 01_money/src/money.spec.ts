c1cca4ae237d2cc0595916224e4fb99328dccc9e
Takuya Abe <takuya.abe@linecorp.com>
2022-03-27 11:52:11 +0900

    feat: Modify class interface

--- 01_money/src/money.spec.ts
+++ 01_money/src/money.spec.ts
@@ -4,8 +4,9 @@ import { Dollar } from './money';
 describe('Dollar', () => {
   test('$5 * 2 = $10', () => {
     const five = new Dollar(5);
-    five.times(2);
-    const expected = 10;
-    expect(five.amount).toBe(expected);
+    let product = five.times(2);
+    expect(product.amount).toBe(10);
+    product = five.times(3);
+    expect(product.amount).toBe(15);
   });
 });
