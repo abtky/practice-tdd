13f3b66cddfad5864ccf883f4336896dbc3ace8c
Takuya Abe <takuya.abe@linecorp.com>
2022-03-27 12:07:51 +0900

    feat: Add new method

--- 01_money/src/money.spec.ts
+++ 01_money/src/money.spec.ts
@@ -2,11 +2,15 @@
 import { Dollar } from './money';
 
 describe('Dollar', () => {
-  test('$5 * 2 = $10', () => {
+  test('multiplication: $5 * 2 = $10', () => {
     const five = new Dollar(5);
     let product = five.times(2);
     expect(product.amount).toBe(10);
     product = five.times(3);
     expect(product.amount).toBe(15);
+  });
+  test('equality', () => {
+    const five = new Dollar(5);
+    expect(five.equals(new Dollar(5))).toBeTruthy();
   });
 });
--- 01_money/src/money.ts
+++ 01_money/src/money.ts
@@ -8,4 +8,9 @@ export class Dollar {
   times(multiplier: number): Dollar {
     return new Dollar(this.amount * multiplier);
   }
+
+  // eslint-disable-next-line no-unused-vars, class-methods-use-this
+  equals(_target: Dollar): boolean {
+    return true;
+  }
 }
