0bada34c4b69f2e026bdd5898269dbc85b25bc3c
Takuya Abe <takuya.abe@linecorp.com>
2022-03-27 14:39:45 +0900

    test: Modify expect return value

--- 01_money/src/money.spec.ts
+++ 01_money/src/money.spec.ts
@@ -4,13 +4,13 @@ import { Money } from './money';
 describe('Dollar', () => {
   test('multiplication: $5 * 2 = $10', () => {
     const five = Money.dollar(5);
-    expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
-    expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
+    expect(five.times(2).equals(Money.dollar(10))).toBe(true);
+    expect(five.times(3).equals(Money.dollar(15))).toBe(true);
   });
   test('equality', () => {
-    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
-    expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
-    expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
   });
   test('currency', () => {
     expect(Money.dollar(1).getCurrency()).toBe('USD');
