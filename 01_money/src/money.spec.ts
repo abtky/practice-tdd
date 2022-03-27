31c065ea858210d1a35f8a6b46cd4a8b9f2018f2
Takuya Abe <takuya.abe@linecorp.com>
2022-03-27 14:57:16 +0900

    feat: Add new class and interface

--- 01_money/src/money.spec.ts
+++ 01_money/src/money.spec.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-unresolved, import/extensions
-import { Money } from './money';
+import { Money, Expression, Bank } from './money';
 
 describe('Dollar', () => {
   test('multiplication: $5 * 2 = $10', () => {
@@ -15,5 +15,11 @@ describe('Dollar', () => {
   test('currency', () => {
     expect(Money.dollar(1).getCurrency()).toBe('USD');
     expect(Money.franc(1).getCurrency()).toBe('CHF');
+  });
+  test('addition', () => {
+    const sum: Expression = Money.dollar(5).plus(Money.dollar(5));
+    const bank: Bank = new Bank();
+    const reduced: Money = bank.reduce(sum, 'USD');
+    expect(Money.dollar(10).equals(reduced)).toBe(true);
   });
 });
--- 01_money/src/money.ts
+++ 01_money/src/money.ts
@@ -1,4 +1,6 @@
-export class Money {
+/* eslint-disable max-classes-per-file */
+export interface Expression {}
+export class Money implements Expression {
   protected amount: number = 0;
 
   protected currency: string = '';
@@ -16,6 +18,10 @@ export class Money {
     return new Money(this.amount * multiplier, this.currency);
   }
 
+  plus(addend: Money): Expression {
+    return new Money(this.amount + addend.amount, this.currency);
+  }
+
   equals(money: Money): boolean {
     const sameCurrency = money.currency === this.currency;
     const sameAmount = money.amount === this.amount;
@@ -28,5 +34,12 @@ export class Money {
 
   static franc(amount: number): Money {
     return new Money(amount, 'CHF');
+  }
+}
+export class Bank {
+  // eslint-disable-next-line class-methods-use-this, no-unused-vars
+  reduce(source: Expression, to: string): Money {
+    console.log({ source, to });
+    return Money.dollar(10);
   }
 }
